2d8f3cd5bcbfc6fad0b2e428b6b38dbafa4f6ee0
Erikgavs <erikgavs@gmail.com>
2025-11-23 19:05:09 +0000

    word_count

--- word_count/src/main.rs
+++ word_count/src/main.rs
@@ -1,10 +1,18 @@
 use std::io;
 
 fn main() {
-    printn!("Put a word to count the letters of the word");
+    println!("Put a word to count the letters of the word\n");
     let mut word = String::new();
     io::stdin()
         .read_line(&mut word)
         .expect("Error reading input");
+    let word = word.trim();
+    let mut count = 0;
+
+    for _ in word.chars(){ // _ => We put this because we don't need to use the variable
+       count += 1;
+    }
+
+    println!("Your word: {} have {} letters", word, count)
 
 }
