e69914d11d8bbc91051bb872a6ee34055f2ed767
Erikgavs <erikgavs@gmail.com>
2025-11-24 22:41:19 +0000

    changes

--- mini-rpg/src/main.rs
+++ mini-rpg/src/main.rs
@@ -1,5 +1,13 @@
 use std::io;
+use std::thread;
+use std::time::Duration;
 use rand::Rng;
+
+// CORRECT \n !!!!!!!
+//
+//
+//
+//
 
 fn main() {
     let mut inventory: Vec<String> = Vec::new(); // created an empty array called inventory to store things in it (we can use this variable in all the matches in main)
@@ -13,7 +21,7 @@ fn main() {
 
         match play {
             "yes" => {
-                println!("Welcome, you are an elf lost in a forest, you lost all your inventory and you are so confused \n The forest is on fire so, what do you want to do? \n\n Run away => command: run \n Stay and search for utils => command: stay "); // intro
+                println!("\n elcome, you are an elf lost in a forest, you lost all your inventory and you are so confused \n The forest is on fire so, what do you want to do? \n\n Run away => command: run \n Stay and search for utils => command: stay "); // intro
                 let mut response1 = String::new();
                 io::stdin()
                     .read_line(&mut response1)
@@ -23,15 +31,16 @@ fn main() {
                     "run" => {
                         let  chance: u8 = rand::thread_rng().gen_range(1..=10);
                         // Maybe change the dice condition cause too dificult
-                        println!("\nYou decided to run but unfortunetly you found a trap and now your life is at risk \n\n now we are going to throw a dice from 0 to 10, if the result is more or equal to 5 you will live \n but if it's less, you will die");
+                        println!("\n You decided to run but unfortunetly you found a trap and now your life is at risk \n\n now we are going to throw a dice from 0 to 10, if the result is more or equal to 5 you will live \n but if it's less, you will die");
+                        thread::sleep(Duration::from_secs(10));
                         if chance >= 5 {
-                            println!("Wow!, you maginifcally solved the problem and didn't die in the trap!, dice: {}", chance);
+                            println!("\n Wow!, you maginifcally solved the problem and didn't die in the trap!, dice: {}", chance);
                         } else {
-                            println!("The god's aren't with you this time, YOU DIED, dice: {}", chance);
+                            println!("\n The god's aren't with you this time, YOU DIED, dice: {}", chance);
                             break;
                         }
 
-                        println!("Now that you passed that trap, you feel a bit hungry but you don't have any food\n\n Do you want to search for some food?\n\n yes => command: yes\n no => command: no");
+                        println!("\n Now that you passed that trap, you feel a bit hungry but you don't have any food\n\n Do you want to search for some food?\n\n yes => command: yes\n no => command: no");
                         let mut response2 = String::new();
                         io::stdin()
                             .read_line(&mut response2)
@@ -50,18 +59,36 @@ fn main() {
                                 let response3 = response3.trim();
                                 match response3 {
                                     "behind" => {
-                                        let chance1: u8 = rand::thread_rng().gen_range(16..=20); // hehehehe :))))
+                                        let chance1: u8 = rand::thread_rng().gen_range(1..=14); // hehehehe :))))
                                         println!("You decide to look back and you found 6 dark elfs surrounding you, they don't seem friendly.\n\n Your life is at risk, a dice from 1 to 20 will be thrown and if the result is equal or more than 15 you will survive\n\n Alea iacta est...");
                                         if chance1 >= 15 {
-                                            println!("You escaped succesfuly! incredible dice: {}", chance1);
+                                            println!("You escaped succesfuly! incredible. dice: {}", chance1);
                                         } else {
-                                            println!("You tried to escape and... you were too solow, the killed you :( dice: {}", chance1);
+                                            println!("You tried to escape and... you were too slow, the killed you :( dice: {}", chance1);
                                             break;
                                         }
                                     }
 
                                     "run" => {
-                                        println!("You decided to escape") // continue here
+                                        println!("You decided to escape, you ran throught the forest and they (we don't know what kind of evil monster was) lost you. \n\n You are more closer to escaping the forest \n\n Now, you can explore or you can continue finding the end of the forest? \n\n Do you want to explore? => command: explore \n\n Do you want to continue finding the end? => command: find"); // continue here
+                                        let mut response4 = String::new();
+                                        io::stdin()
+                                            .read_line(&mut response4)
+                                            .expect("Error reading input");
+                                        let response4 = response4.trim();
+                                        match response4 {
+                                            "explore" => {
+                                                println!("You decided to explore the area and you found a MEGA SWORD, when you touch the sword, the sword starts to shine, you found a Phantom Sword!");
+                                                inventory.push("Phantom Sword".to_string());
+                                                println!("This is your inventory now: {:?}", inventory);
+                                                println!("Exploring the area, you also found a chest but you don't have the key ");
+
+                                            }
+
+                                            _=>{
+                                                println!("Invalid input")
+                                            }
+                                        }
                                     }
 
                                     _=> {
@@ -72,7 +99,6 @@ fn main() {
                             }
                             //////////////////////////////////////////////////
                             "no" => {
-
                             }
                             _=>{
                                 println!("Invalid input")
